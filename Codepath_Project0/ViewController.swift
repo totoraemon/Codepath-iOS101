c604e27601eea9dfb193375b01328010dae20649
Joseline Ly <92823715+totoraemon@users.noreply.github.com>
2025-05-11 23:20:03 -0700

    added all iOS101 prework files to repo

---
+++ Codepath_Project0/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Codepath_Project0
+//
+//  Created by Joseline  Ly on 5/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundTapped(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+
+    @IBOutlet var changeBackgroundColor: UIButton!
+    func changeColor() -> UIColor {
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+}
+
+
